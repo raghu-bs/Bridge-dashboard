14302b36dd0763cc4b463971453e26990c7a8499
raghu-bs <47857856+raghu-bs@users.noreply.github.com>
2019-04-10 15:49:05 +0530

    order ui

--- src/app/home/orders/order-overview/order-overview.component.ts
+++ src/app/home/orders/order-overview/order-overview.component.ts
@@ -1,15 +1,106 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+];
 @Component({
   selector: 'app-order-overview',
   templateUrl: './order-overview.component.html',
   styleUrls: ['./order-overview.component.scss']
 })
 export class OrderOverviewComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+  piechart1: any;
+  piechart2: Chart;
 
   constructor() { }
 
   ngOnInit() {
+    this.donutPieChart();
+  }
+
+  donutPieChart() {
+    this.piechart1 = new Chart({
+      chart: {
+        type: 'pie'
+      },
+      title: {
+        text: 'Load'
+      },
+      yAxis: {
+        title: {
+          text: 'Total percent market share'
+        }
+      },
+      plotOptions: {
+        pie: {
+          shadow: false
+        }
+      },
+      tooltip: {
+        formatter: function () {
+          return '<b>' + this.point.name + '</b>: ' + this.y + ' %';
+        }
+      },
+      series: [{
+        name: 'Browsers',
+        data: [['Firefox', 6], ['MSIE', 4], ['Chrome', 7]],
+        size: '60%',
+        innerSize: '20%',
+        showInLegend: true,
+        dataLabels: {
+          enabled: false
+        }, type: undefined
+      }]
+    });
+
+    this.piechart2 = new Chart({
+      chart: {
+        renderTo: 'container',
+        type: 'pie'
+      },
+      title: {
+        text: 'Redeemed'
+      },
+      yAxis: {
+        title: {
+          text: 'Total percent market share'
+        }
+      },
+      plotOptions: {
+        pie: {
+          shadow: false
+        }
+      },
+      tooltip: {
+        formatter: function () {
+          return '<b>' + this.point.name + '</b>: ' + this.y + ' %';
+        }
+      },
+      series: [{
+        name: 'Browsers',
+        data: [['Firefox', 6], ['MSIE', 4], ['Chrome', 7]],
+        size: '60%',
+        innerSize: '20%',
+        showInLegend: true,
+        dataLabels: {
+          enabled: false
+        }, type: undefined
+      }]
+    });
   }
 
 }
