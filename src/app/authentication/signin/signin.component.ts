eccc2f67f096f939248c16c1f6374b86872cb910
vanitha91 <vanitha.l@tvsnext.in>
2019-04-11 11:17:57 +0530

    login screen web and mob responsive and common services

--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-signin',
@@ -10,18 +13,20 @@ export class SigninComponent implements OnInit {
   public username: string;
   public password: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private loginService: LoginService) { }
 
   ngOnInit() {
   }
-    login() : void {
+    login() {
     if(this.username == 'admin' && this.password == 'admin'){
       let loginCredentials = {email: this.username, password: this.password};
-      // this.loginService.login(loginCredentials).subscribe(data => {
-      //   console.log(data);
-      // });
-     this.router.navigate(['home/dashboard']);
-    }else {
+      this.loginService.login(loginCredentials).subscribe(data => {
+        console.log(data);
+        if(data) {
+          this.router.navigate(['home/dashboard']);
+        }
+      });
+    } else {
       alert("Invalid credentials");
     }
   }
